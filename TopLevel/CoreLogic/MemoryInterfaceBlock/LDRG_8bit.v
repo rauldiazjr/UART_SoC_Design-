a5be438fd09ef91d849477e430cb0de5d0191036
RaulDiazjr <raul.diaz91@live.com>
2016-01-27 14:53:51 -0800

    Synch 8 bit Register

--- TopLevel/CoreLogic/MemoryInterfaceBlock/LDRG_8bit.v
+++ TopLevel/CoreLogic/MemoryInterfaceBlock/LDRG_8bit.v
@@ -7,10 +7,10 @@ module LDRG_8bit(Clk, Rstb, D, en, Q);
      input      [7:0] D; 
      output reg [7:0] Q; 
 
-	always @(posedge Clk, negedge Rstb)
-		if(Rstb == 1'b0)  Q <= 8'b0; else
-		if(en)		   Q <= D;    else
-					   Q <= Q; 
-		
+   always @(posedge Clk, negedge Rstb)
+      if(Rstb == 1'b0)  Q <= 8'b0; else
+      if(en)            Q <= D;    else
+                        Q <= Q; 
+      
 
 endmodule
