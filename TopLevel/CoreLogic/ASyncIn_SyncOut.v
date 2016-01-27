b459a753184b659339340e610d9e5957c01dd166
RaulDiazjr <raul.diaz91@live.com>
2016-01-27 15:15:59 -0800

    AISO synchronize module for hardware reset

--- TopLevel/CoreLogic/ASyncIn_SyncOut.v
+++ TopLevel/CoreLogic/ASyncIn_SyncOut.v
@@ -10,21 +10,21 @@
 * This module uses 2 flip-flop registers to remove metastability which may occur 
 * if the setup time for the first ff is violated from a hardware mechanical debounce.
 * Ultimitely this module will synchronize the system with an active low reset. 
-*		i.e If a mech. reset is asserted, the output will traverse a 0 through ff1, and
-*		on the next clk, output a 0. 
-*		else the output remains 0. 
+*      i.e If a mech. reset is asserted, the output will traverse a 0 through ff1, and
+*      on the next clk, output a 0. 
+*      else the output remains 0. 
 **********************************************************************************/
 module ASyncIn_SyncOut(Clk, async_rst, sync_rst);
-	input 		Clk, async_rst; 
-	output reg  sync_rst; 
-	reg 			ff1; 
-	
+   input       Clk, async_rst; 
+   output reg  sync_rst; 
+   reg         ff1; 
+   
 /*************Conditional block******************/
-	always @(posedge Clk, negedge async_rst)  		
-		if(async_rst == 1'b0) ff1 <= 1'b1;  	
-		else				  ff1 <= 1'b0; 
+   always @(posedge Clk, negedge async_rst)        
+      if(async_rst == 1'b0) ff1 <= 1'b1;     
+      else                  ff1 <= 1'b0; 
 
-	always @(posedge Clk)
-		sync_rst <= ff1; 
-		
+   always @(posedge Clk)
+      sync_rst <= ff1; 
+      
 endmodule
